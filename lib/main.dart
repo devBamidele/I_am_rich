87160a0ccfead8c866f9b500c148dc3409189d12
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-05-03 12:48:26 +0100

    Added an asset image

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,7 @@ void main() {
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage(// Fetch an image from internet with a Url
-                'https://uploads-ssl.webflow.com/5f841209f4e71b2d70034471/6078b650748b8558d46ffb7f_Flutter%20app%20development.png'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
